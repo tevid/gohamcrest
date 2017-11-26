6bcb1fda64ae2d4b51ba22a25a2e2ea603160b6a
joezou <yixian.zou@gmail.com>
2017-11-26 21:54:39 +0800

    refactor number

--- number.go
+++ number.go
@@ -1,42 +1,29 @@
 package tug
 
-import (
-	"fmt"
-)
-
 type greaterThanMatcher struct {
-	reason string
-	expected interface{}
+	BaseMatcher
 }
 
 func (this *greaterThanMatcher) Match(actual interface{}) bool {
-	validateParamType(actual,this.expected)
+	validateParamType(actual,this.Expected)
 
 	switch actual.(type) { //多选语句switch
 	case float64:
-		return actual.(float64)>this.expected.(float64)
+		return actual.(float64)>this.Expected.(float64)
 	case float32:
-		return actual.(float32)>this.expected.(float32)
+		return actual.(float32)>this.Expected.(float32)
 	case int:
-		return actual.(int)>this.expected.(int)
+		return actual.(int)>this.Expected.(int)
 	}
 
 	return false
 }
 
-func (this *greaterThanMatcher)FailReason(actual interface{})string {
-	return fmt.Sprintf(this.reason,actual,LOGIC_NOT,this.expected)
-}
-
-func (this *greaterThanMatcher)NegationFailReason(actual interface{})string {
-	return fmt.Sprintf(this.reason,actual,EMPTY,this.expected)
-}
-
 func GreaterThan(expected interface{}) Matcher {
-	return &greaterThanMatcher{
-		expected:expected,
-		reason:"%v is %s greater than %v(excepted)",
-	}
+	matcher := &greaterThanMatcher{}
+	matcher.Expected=expected
+	matcher.Reason="%v is %s greater than %v(excepted)"
+	return matcher
 }
 
 func LessThanOrEquals(expected interface{}) Matcher {
@@ -44,38 +31,29 @@ func LessThanOrEquals(expected interface{}) Matcher {
 }
 
 type lessThanMatcher struct {
-	reason string
-	expected interface{}
+	BaseMatcher
 }
 
 func (this *lessThanMatcher) Match(actual interface{}) bool {
-	validateParamType(actual,this.expected)
+	validateParamType(actual,this.Expected)
 
 	switch actual.(type) { //多选语句switch
 	case float64:
-		return actual.(float64)<this.expected.(float64)
+		return actual.(float64)<this.Expected.(float64)
 	case float32:
-		return actual.(float32)<this.expected.(float32)
+		return actual.(float32)<this.Expected.(float32)
 	case int:
-		return actual.(int)<this.expected.(int)
+		return actual.(int)<this.Expected.(int)
 	}
 
 	return false
 }
 
-func (this *lessThanMatcher)FailReason(actual interface{})string {
-	return fmt.Sprintf(this.reason,actual,LOGIC_NOT,this.expected)
-}
-
-func (this *lessThanMatcher)NegationFailReason(actual interface{})string {
-	return fmt.Sprintf(this.reason,actual,EMPTY,this.expected)
-}
-
 func LessThan(expected interface{}) Matcher {
-	return &lessThanMatcher{
-		expected:expected,
-		reason:"%v is %s less than %v(excepted)",
-	}
+	matcher := &lessThanMatcher{}
+	matcher.Expected=expected
+	matcher.Reason="%v is %s less than %v(excepted)"
+	return matcher
 }
 
 func GreaterThanOrEquals(expected interface{}) Matcher {

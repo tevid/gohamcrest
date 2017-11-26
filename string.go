9f8cccb01fbf425f2bd9190c4860382f9f20b3cb
joezou <yixian.zou@gmail.com>
2017-11-26 21:16:32 +0800

    refactor startwith

--- string.go
+++ string.go
@@ -1,35 +1,25 @@
 package tug
 
 import (
-	"fmt"
 	"strings"
 )
 
 type startWith struct {
-	expected string
-	reason string
+	BaseMatcher
 }
 
 func (this *startWith) Match(actual interface{}) bool {
 	if str, ok := actual.(string); ok {
-		return strings.HasPrefix(str, this.expected)
+		return strings.HasPrefix(str, this.Expected)
 	}
 	return false
 }
 
-func (this *startWith)FailReason(actual interface{})string {
-	return fmt.Sprintf(this.reason,actual,LOGIC_NOT,this.expected)
-}
-
-func (this *startWith)NegationFailReason(actual interface{})string {
-	return fmt.Sprintf(this.reason,actual,this.expected)
-}
-
 func StartWith(expected string) Matcher {
-	return &startWith{
-		expected:expected,
-		reason:"%v %s start with %v",
-		}
+	matcher := &endWith{}
+	matcher.Expected=expected
+	matcher.Reason="%v %s start with %v"
+	return matcher
 }
 
 type endWith struct {

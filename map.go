3efbeb207594086bcf3dd5035759d1d72e9d58ea
joezou <yixian.zou@gmail.com>
2018-10-18 23:37:07 +0800

    add map assert

---
+++ map.go
@@ -0,0 +1,63 @@
+package gohamcrest
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type emptyList struct {
+	reason string
+}
+
+func (this *emptyList) Match(actual interface{}) bool {
+	list, ok := actual.(*list.List)
+	if ok {
+		return list==nil||list.Len()== 0
+	}else{
+		panic(fmt.Sprintf("type not match List.list,%T(actual)",list))
+	}
+	return false
+}
+
+func (this *emptyList)FailReason(actual interface{})string {
+	return fmt.Sprintf(this.reason,actual,LOGIC_NOT)
+}
+
+func (this *emptyList)NegationFailReason(actual interface{})string {
+	return fmt.Sprintf(this.reason,actual)
+}
+
+func EmptyList() Matcher {
+	return &emptyList{
+		reason:"list is %s nil, %v(actual)",
+	}
+}
+
+type hasItems struct {
+	BaseMatcher
+}
+
+func (this *hasItems) Match(actual interface{}) bool {
+	list, ok := actual.(*list.List)
+	if !ok {
+		panic(fmt.Sprintf("type not match List.list,%T(actual)",list))
+	}
+
+	flag := false
+
+	for e := list.Front(); e != nil; e = e.Next() {
+		if e.Value == this.Expected {
+			flag = true
+			break
+		}
+	}
+
+	return flag
+}
+
+func HasItems(expected interface{}) Matcher {
+	matcher := &hasItems{}
+	matcher.Expected=expected
+	matcher.Reason="List: %v is %s contain %v(excepted)"
+	return matcher
+}
---
+++ map_test.go
@@ -0,0 +1,30 @@
+package gohamcrest
+
+import (
+	"testing"
+	"container/list"
+)
+
+func TestEmptyList(t *testing.T) {
+	l := list.New()
+	Assert(t,l,EmptyList())
+}
+
+func TestNotEmptyList(t *testing.T) {
+	l := list.New()
+	l.PushBack("joe")
+	Assert(t,l,Not(EmptyList()))
+}
+
+//func TestNotEmptyListError(t *testing.T) {
+//	Assert(t,"",Not(EmptyList()))
+//}
+
+func TestHasStringItems(t *testing.T) {
+	l := list.New()
+	l.PushBack("joe")
+	Assert(t,l,HasItems("joe"))
+	//Assert(t,l,HasItems("joe1"))
+}
+
+
